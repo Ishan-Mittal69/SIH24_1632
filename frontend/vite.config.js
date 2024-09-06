5f7a0feb86297ec55864df1c924d4c2f6923f0de
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-09-07 01:50:56 +0530

    backend deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://pearlcrest.onrender.com',
+      '/api': 'https://sih24-1632.onrender.com',
     },
   },
   plugins: [react()],
